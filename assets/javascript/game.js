8b8cfac4e41152a2ad0166116991c3f76fe88661
Zach <zachariahthomas54@yahoo.com>
2019-10-26 00:46:09 -0400

    Javascript for text population changed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,7 +19,7 @@ document.onkeyup = function () {
             letter = "";
         }
         if (userselect !== computerselect) {
-            letter = letter + userselect + ", ";
+            letter =  letter + userselect  + ", " ;
             guessLeft--;
 
             if (guessLeft == 0) {
